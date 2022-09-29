65b52cf2e856ddd71b403dd7b7fa35ac6a537211
Naviralobo <majel.josephine>
2022-09-29 10:34:55 +0530

    Retry and cancelButton

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -6,39 +6,61 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  // function fetchMoviesHandler() {
-  //   fetch("https://swapi.dev/api/films")
-  //     .then((response) => {
-  //       return response.json();
-  //     })
-  //     .then((data) => {
-  //       const transformedMovies = data.results.map((movieData) => {
-  //         return {
-  //           id: movieData.episode_id,
-  //           title: movieData.title,
-  //           openingText: movieData.opening_crawl,
-  //           releaseDate: movieData.release_date,
-  //         };
-  //       });
-  //       setMovies(transformedMovies);
-  //     });
-  // }
+  const [error, setError] = useState(null);
+  const [retry, setRetry] = useState(false);
+
+  useEffect(() => {
+    if (retry === true) {
+      var id = setInterval(fetchMoviesHandler, 5000);
+    } else {
+      clearInterval(id);
+    }
+    return () => clearInterval(id);
+  }, [retry]);
+
   async function fetchMoviesHandler() {
     setIsLoading(true);
-    const response = await fetch("https://swapi.dev/api/films");
-    const data = await response.json();
+    setError(null);
 
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
+    try {
+      const response = await fetch("https://swapi.dev/api/film");
+      if (!response.ok) {
+        throw new Error("Something went Wrong...Retrying");
+      }
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+      setRetry(true);
+    }
     setIsLoading(false);
   }
+
+  let content = <p>found no movies..</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
+  const cancelRetryHandler = () => {
+    setRetry(false);
+  };
 
   return (
     <React.Fragment>
@@ -46,9 +68,8 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && <MoviesList movies={movies} />}
-        {isLoading && <p>Loading...</p>}
-        {!isLoading && movies.length == 0 && <p>No movies to show</p>}
+        {content}
+        <button onClick={cancelRetryHandler}>Cancel</button>
       </section>
     </React.Fragment>
   );
